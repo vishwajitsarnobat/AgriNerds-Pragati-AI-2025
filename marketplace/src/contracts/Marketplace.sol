1e3b28e831b5a3eb3de61fba0758d580ab2136ac
Vishwajit <sambhaji2772@gmail.com>
2025-04-18 19:35:27 +0530

    Initial contract

---
+++ marketplace/src/contracts/Marketplace.sol
@@ -0,0 +1,93 @@
+pragma solidity ^0.8.0
+
+contract Marketplace {
+    string public name;
+    uint public contractCount = 0;
+    mapping(uint => Contract) public contracts;
+
+    struct Contract {
+        uint id;
+        string crop;
+        uint quantity; // quantity in units
+        string unit; // unit for quantity (tonnes, kilos, dozens, etc.)
+        uint price; // price per defined unit
+        uint status;
+        // 0 -> expired
+        // 1 -> initiated, but not accepted yet
+        // 2 -> ongoing
+        // 3 -> completed
+        address owner; // the one who initiates the contract
+        string ownerName;
+        address promisee; // the one who accepts the contract
+        string promiseeName;
+        uint256 creationTime;
+        string date;
+        string insurance;
+    }
+
+    event contractCreated (
+        uint id,
+        string crop,
+        uint quantity,
+        string unit,
+        uint price,
+        uint status,
+        address owner,
+        string ownerName,
+        address promisee,
+        string promiseeName,
+        uint256 creationTime,
+        string date,
+        string insurance
+    );
+
+    constructor() public {
+        name = "AgriNerds Marketplace";
+    }
+
+    struct ContractInputData {
+        string crop;
+        uint quantity;
+        string unit;
+        uint price;
+        string ownerName;
+        string date; // Expecting "DDMMYYYY"
+        string insurance;
+    }
+
+    function createContract(ContractInputData memory _input) public {
+        // check parameters first
+        
+        require(_input.quantity > 0, "Quantity must be greater than zero");
+        require(_input.price > 0, "Price must be greater than zero");
+        require(bytes(_input.date).length == 8, "Invalid date format: requires 8 chars (DDMMYYYY)");
+
+        // create contract
+        uint contractId = contractCount;
+        uint256 currTime = block.timestamp;
+
+        Contract memory newContract;
+
+        newContract.id = contractId;
+        newContract.crop = _input.crop;
+        newContract.quantity = _input.quantity;
+        newContract.unit = _input.unit;
+        newContract.price = _input.price;
+        newContract.status = 1;
+        newContract.owner = msg.sender;
+        newContract.ownerName = _input.ownerName;
+        newContract.promisee = address(0);
+        newContract.promiseeName = '';
+        newContract.creationTime = currTime;
+        newContract.date = _input.date;
+        newContract.insurance = _input.insurance;
+
+        contracts[contractId] = newContract;
+
+        // increase contractCount
+        contractCount++;
+
+        // event
+        emit contractCreated(contractCount, _input.crop, _input.quantity, _input.unit, _input.price, 1, msg.sender, _input.ownerName, address(0), 'null', currTime, _input.date, _input.insurance);
+    }
+}
---
+++ marketplace/test/Marketplace.test.js
@@ -0,0 +1,59 @@
+const { assert } = require("chai")
+
+const Marketplace = artifacts.require('./Marketplace.sol')
+
+contract('Marketplace', (accounts) => {
+    let marketplace
+
+    before(async () => {
+        marketplace = await Marketplace.deployed()
+    })
+
+    describe('deployment', async () => {
+        it('deployment', async () => {
+            const address = await marketplace.address
+            assert.notEqual(address, 0x0)
+            assert.notEqual(address, '')
+            assert.notEqual(address, null)
+            assert.notEqual(address, undefined)
+        })
+    })
+
+    it('has correct name', async () => {
+        const name = await marketplace.name()
+        assert.equal(name, "AgriNerds Marketplace") 
+    })
+
+    describe('contract', async () => {
+        let testContract;
+
+        const initContractCount = await marketplace.contractCount();
+
+        before(async () => {
+            testContract = await marketplace.createContract('potato', 23, 'tonnes', 34000, 'Pepsico', 12, 7, 2025, 'PM Fasal Vima Yojana');
+        })
+
+        it('is contractCount increased', async () => {
+            const newContractCount = marketplace.contractCount();
+            assert.notEqual(newContractCount - initContractCount, 0);
+        })
+
+        it('is contract created properly', async() => {
+            assert.notEqual(testContract.crop(), '');
+            assert(testContract.quantity() > 0);
+            assert.notEqual(testContract.unit(), '');
+            assert(testContract.price() > 0);
+            assert(testContract.status() == 1);
+            assert.notEqual(testContract.owner(), 0x0);
+            assert.notEqual(ownerName, '');
+            assert(creationTime > 0);
+            assert.notEqual(testContract.date.day(), '');
+            assert.notEqual(insurance, '');
+        })
+    })
+
+    it('has correct name', async () => {
+        const name = await marketplace.name()
+        assert.equal(name, "AgriNerds Marketplace") 
+    })
+})
